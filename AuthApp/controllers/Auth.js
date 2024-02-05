62615a211633bae4ef23cad1f5f7d096fa671872
Pritam kumar <92274739+pritamchaudhary@users.noreply.github.com>
2024-02-05 11:53:23 +0530

    complete login and signup functionality

--- AuthApp/controllers/Auth.js
+++ AuthApp/controllers/Auth.js
@@ -1,5 +1,7 @@
 const bcrypt = require("bcrypt");
 const User = require("../models/User");
+const jwt = require("jsonwebtoken");
+require("dotenv").config();
 
 // signup route handler
 exports.signup = async (req, res) => {
@@ -40,6 +42,76 @@ exports.signup = async (req, res) => {
     return res.status(500).json({
       success: true,
       message: "User cannot be registered please try again later",
+    });
+  }
+};
+
+// login
+exports.login = async (req, res) => {
+  try {
+    // data fetch
+    const { email, password } = req.body;
+    // validation on email and password
+    if (!email || !password) {
+      return res.status(400).json({
+        success: false,
+        message: "Please fill all the details carefully",
+      });
+    }
+    // check for registered user
+    let user = await User.findOne({ email });
+    // if not a registered user
+    if (!user) {
+      return res.status(401).json({
+        success: false,
+        message: "User is not registered",
+      });
+    }
+    const payload = {
+      email: user.email,
+      id: user._id,
+      role: user.role,
+    };
+    // verify password & generate a JWT token
+    if (await bcrypt.compare(password, user.password)) {
+      // password match
+      let token = jwt.sign(payload, process.env.JWT_SECRET, {
+        expiresIn: "2h",
+      });
+
+      user = user.toObject();
+      user.token = token;
+      user.password = undefined;
+
+      const options = {
+        expires: new Date(Date.now() + 3 * 24 * 60 * 60 * 1000),
+        httpOnly: true,
+      };
+      res.cookie("token", token, options).status(200).json({
+        success: true,
+        token,
+        user,
+        message: "User Logged in successfully",
+      });
+
+      // res.status(200).json({ 
+      //   success: true,
+      //   token,
+      //   user,
+      //   message: "User Logged in successfully",
+      // });
+    } else {
+      // password not match
+      return res.status(403).json({
+        success: false,
+        message: "Password Incorrect",
+      });
+    }
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json({
+      success: false,
+      message: "Login Failure",
     });
   }
 };
