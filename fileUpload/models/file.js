4f989897c3c8be0884a067736a97db2be2540a42
Pritam kumar <92274739+pritamchaudhary@users.noreply.github.com>
2024-03-20 10:36:33 +0530

    completed file upload concept

--- fileUpload/models/file.js
+++ fileUpload/models/file.js
@@ -21,13 +21,26 @@ const fileSchema = new mongoose.Schema({
 fileSchema.post("save", async function(doc) {
     try {
         // transporter
-        let transporter = nodemailer.transporter({
+        // TODO: shift this configuration under /config folder
+        let transporter = nodemailer.createTransport({
             host:process.env.MAIL_HOST,
             auth:{
                 user:process.env.MAIL_USER,
                 pass:process.env.MAIL_PASS,
             },
+            tls: {
+                // do not fail on invalid certs
+                rejectUnauthorized: false
+            },
         })
+        // send mail
+        let info = await transporter.sendMail({
+            from:`pritam`,
+            to:doc.email,
+            subject:"New File uploaded on Cloudinary",
+            html:`<h2>Hello Jee</h2> <p>File Uploaded</p> View here: <a href="${doc.imageUrl}">${doc.imageUrl}</a>`,
+        })
+        console.log("INFO", info);
     } catch (error) {
         console.error(error);
     }
